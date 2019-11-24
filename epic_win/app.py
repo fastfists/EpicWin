f4bc1d6afcc232ccc4e2eacf2633bc80c071d831
Denzell Ford <fdenzell@gmail.com>
2019-11-24 23:12:49 +0000

    fixed sekazi bug

--- epic_win/app.py
+++ epic_win/app.py
@@ -7,6 +7,7 @@ def create_app(config_object='epic_win.settings') -> Flask:
 
     app = Flask(__name__)
     app.config.from_object(config_object)
+    register_extensions(app)
 
     user.init_app(app)
     public.init_app(app)
@@ -14,12 +15,13 @@ def create_app(config_object='epic_win.settings') -> Flask:
     return app
 
 def register_extensions(app):
-    from epic_win.ext import migrate, ma, admin, db, sekazi, login_manager, bcrypt
+    from epic_win.ext import migrate, ma, admin, db, sekazi, login_manager, bcrypt, sekazi
 
     admin.init_app(app)
     db.init_app(app)
     db.app = app
 
+    sekazi.init_app(app)
     login_manager.init_app(app)
     sekazi.init_app(app)
     ma.init_app(app)
