589d5bd57a635a97c444c421404df45af8f60869
Denzell Ford <fdenzell@gmail.com>
2019-11-27 22:53:51 +0000

    updated sekazi version

--- epic_win/public/views.py
+++ epic_win/public/views.py
@@ -4,7 +4,11 @@ public = Blueprint('public', __name__)
 
 @public.route('/')
 def index():
-    return render_template("public/index.html")
+    item = {"name": "Nike Air Hurricanes",
+            "price": 60,
+            "image_url": "/static/images/item.png"
+            }
+    return render_template("public/index.html", items=[item for i in range(5)])
 
 @public.route('/about')
 def about():
