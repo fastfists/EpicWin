b2a7b056d0a2757920a504e57c79b9bb1deeaf60
Denzell Ford <fdenzell@gmail.com>
2019-12-11 23:39:18 +0000

    Rendered shop page using javascript

--- epic_win/static/js/products.js
+++ epic_win/static/js/products.js
@@ -1,6 +1,22 @@
-products = await fetch('/v1/search?page=1')["products"];
-for (int i = 0; i < products.lenght; i++){
-    product = products[i];
+import render_item from './items.js'
+
+async function render_products() {
+
+    let response = await fetch('/v1/search?page=1');
+    console.log(response)
+    let json = await response.json();
+    let products = json["products"];
+    let html = ""
+    for (let i = 0; i < products.length; i++){
+        let product = products[i];
+        html += render_item(product);
+    }
+//
+    document.querySelector("#shop-list").innerHTML = html;
+
 
 }
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    render_products();
+});
